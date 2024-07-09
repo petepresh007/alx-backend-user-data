80d3b8d64ddd01ac588ecec0041220ccdaea3c0b
peter precious <peteprecious370@gmail.com>
2024-07-09 15:57:42 -0700

    tabinko gabalaya

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -39,3 +39,24 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(
+             self, decoded_base64_authorization_header: str
+             ) -> (str, str):
+        """
+        Extracts the user email and password from the Base64 decoded value.
+
+        Args:
+            decoded_base64_authorization_header (str): The decoded.
+
+        Returns:
+            tuple: The user email and password, or (None, None) if invalid.
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        user_credentials = decoded_base64_authorization_header.split(':', 1)
+        return user_credentials[0], user_credentials[1]
