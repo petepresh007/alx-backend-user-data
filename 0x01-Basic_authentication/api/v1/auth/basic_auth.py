e26c3c551b48449f0f19ef359ab92c4d809d40b0
peter precious <peteprecious370@gmail.com>
2024-07-11 11:49:41 -0700

    auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 '''basic autentication'''
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from api.v1.views.users import User
 
 
 class BasicAuth(Auth):
@@ -60,3 +62,21 @@ class BasicAuth(Auth):
             return None, None
         user_credentials = decoded_base64_authorization_header.split(':', 1)
         return user_credentials[0], user_credentials[1]
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str
+            ) -> TypeVar('User'):
+        '''method to authenticate credentials'''
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        users = User.search({'email': user_email})
+        if not users:
+            return None
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
