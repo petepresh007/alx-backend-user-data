2869831cd38988566edeec7cfbde47127034c3a0
peter precious <peteprecious370@gmail.com>
2024-07-18 09:22:38 -0700

    login with authentication

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''A simple Flask app with user authentication features.'''
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -25,6 +25,22 @@ def register():
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=['POST'], strict_slashes=False)
+def login():
+    '''login valid users'''
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    user = AUTH.valid_login(email, password)
+    if not user:
+        abort(401)
+    else:
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
 
 
 if __name__ == "__main__":
