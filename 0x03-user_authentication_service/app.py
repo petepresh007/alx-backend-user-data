83097565b65c59567e077b083dc74548d1c08d87
peter precious <peteprecious370@gmail.com>
2024-07-18 05:27:35 -0700

    hopeful fix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -6,7 +6,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['GET'], strict_slashes=False)
 def index() -> str:
     """default page"""
     return jsonify({"message": "Bienvenue"})
