14ffe5e3360bd36fb866910bf2c756013de31fe8
peter precious <peteprecious370@gmail.com>
2024-07-18 11:30:00 -0700

    logout fix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''A simple Flask app with user authentication features.'''
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, url_for
 from auth import Auth
 
 
@@ -50,10 +50,15 @@ def logout() -> str:
     """
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
+
+    if not session_id:
+        abort(403)
+
     if user is None:
         abort(403)
+
     AUTH.destroy_session(user.id)
-    return redirect("/")
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
