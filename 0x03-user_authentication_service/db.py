8aad41d5e5a5d2015f88be734f11824845d36fd7
peter precious <peteprecious370@gmail.com>
2024-07-18 02:52:39 -0700

    db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
-"""DB module
+"""DB module to handle database functionalities
 """
 from sqlalchemy import create_engine, tuple_
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.session import Session
 
 
 from user import Base, User
 
 
 class DB:
-    """DB class
+    """DB class to handle db functionalities
     """
 
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
